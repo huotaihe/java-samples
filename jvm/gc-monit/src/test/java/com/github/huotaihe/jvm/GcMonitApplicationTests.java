6ae09a3e12bc28102430dc433138b2017d846931
寒溪 <taihe.hth@alibaba-inc.com>
2022-04-22 22:42:17 +0800

    修复部分属性方法没有的问题

---
+++ jvm/gc-monit/src/test/java/com/github/huotaihe/jvm/GcMonitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.huotaihe.jvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcMonitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
